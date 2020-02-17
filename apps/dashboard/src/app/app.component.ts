324732535b8a5922a90c2a5b789928c5fbe78536
Elijah de Gouveia <elijah@venturplex.com>
2020-02-17 10:10:12 -0700

    initial commit

---
+++ apps/dashboard/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mdv-twenty-one-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'dashboard';
+}
